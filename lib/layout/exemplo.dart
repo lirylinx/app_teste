63f84a05464e12499b60887ef573f0277ecd75be
liry.linx <dercioguirruta44@gmail.com>
2020-12-18 10:51:18 +0200

    flutter layout demo

--- lib/layout/exemplo.dart
+++ lib/layout/exemplo.dart
@@ -41,7 +41,7 @@ Widget layoutExemplo1 = Container(
               Icons.star,
               color: Colors.red[500],
             ),
-            Text('42')
+            Text('45')
           ],
         ),
       );
@@ -108,13 +108,67 @@ Column _buildButtonColumn(Color color, IconData icon, String label) {
   );
 }
 
-// Color color = Theme.of ( context). primaryColor;
-// Widget buttonSection = Container (
-//   child: Row(
-//     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//     children: [
-//       _buildButtonColumn(color, icon, label)
-//     ],
-//   )
-// )
 
+
+
+Widget buttonSection (Color color){
+  return  Container (
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      _buildButtonColumn(color, Icons.call, 'CALL'),
+      _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+      _buildButtonColumn(color, Icons.share, 'SHARE'),
+
+    ],
+  )
+);
+}
+
+
+Widget textSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+  ),
+);
+
+
+class LayoutMyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+  Color color = Theme.of ( context). primaryColor;
+
+      return MaterialApp(
+        title: 'app',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(' Flutter layout Demo'),
+          ),
+          body: Column(
+            children: [
+              Image.asset(
+                'images/lake.jpg',
+                width: 600,
+                height: 240,
+                fit: BoxFit.cover
+              ),
+              layoutExemplo1,
+              buttonSection(color),
+              textSection
+            ]
+          )
+        )
+      );
+  
+  }
+
+
+}
